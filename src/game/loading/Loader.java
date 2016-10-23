47a1396e847891a66003547a74106be390ad9ad5
MartinKitten <mro.ar@yandex.ru>
2016-10-23 17:55:51 +0300

    Added texture loading via Slick-Util.

--- src/game/loading/Loader.java
+++ src/game/loading/Loader.java
@@ -1,5 +1,7 @@
 package game.loading;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 import java.util.ArrayList;
@@ -10,13 +12,17 @@ import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
 
 import game.rendering.RawModel;
+import game.util.LogUtil;
 
 public class Loader {
 	
 	private List<Integer> vaos = new ArrayList<>();
 	private List<Integer> vbos = new ArrayList<>();
+	private List<Integer> textures = new ArrayList<>();
 	
 	public RawModel loadToVAO(float[] positions, int[] indices){
 		int vaoID = createVAO();
@@ -26,13 +32,43 @@ public class Loader {
 		return new RawModel(vaoID, indices.length);
 	};
 	
+	public int loadTexture(String name){
+		Texture texture = null;
+		int textureId = 0;
+		try{
+			texture = TextureLoader.getTexture("PNG", new FileInputStream("assets/texture/" + name + ".png"));
+			textureId = texture.getTextureID();
+			textures.add(textureId);
+			return textureId;
+		} catch (FileNotFoundException e){
+			LogUtil.println("Texture " + name + " is not found!");
+			e.printStackTrace();
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		
+		try{
+			texture = TextureLoader.getTexture("PNG", new FileInputStream("assets/texture/default.png"));
+			textureId = texture.getTextureID();
+			textures.add(textureId);
+			return texture.getTextureID();
+		} catch(Exception e){
+			LogUtil.println("Probably you have deleted default.png. Please bring it back or suck my dick.");
+			e.printStackTrace();
+		}
+		System.exit(1);
+		return textureId;
+	}
+	
 	public void dispose(){
 		for(int vao : vaos){
 			GL30.glDeleteVertexArrays(vao);
 		}
 		for(int vbo : vbos){
 			GL15.glDeleteBuffers(vbo);
-			
+		}
+		for(int texture : textures){
+			GL11.glDeleteTextures(texture);
 		}
 	}
 	
